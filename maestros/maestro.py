979abffa7b5abdb601fbe8e6fb4c7605483d439d
Aneli <anelitj@gmail.com>
2024-09-18 22:41:10 -0600

    yap

--- maestros/maestro.py
+++ maestros/maestro.py
@@ -7,4 +7,9 @@ class Maestro:
     rfc: str
     sueldo: float
 
-
+    def __init__(self, nombre:str, apellido:str, rfc:str, sueldo:float):
+        self.numero_control= ""
+        self.nombre=nombre
+        self.apellido=apellido
+        self.rfc=rfc
+        self.sueldo=sueldo
